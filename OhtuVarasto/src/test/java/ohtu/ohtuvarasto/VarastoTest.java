f97f64bcca3ccb861e222185f8d789cb8a96dfc3
samtuhka <samuel.tuhkanen@helsinki.fi>
2015-03-15 17:54:50 +0200

    korjaa testi

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -82,7 +82,7 @@ public class VarastoTest {
     public void otaVarastostaNegatiivnen() {
         varasto.lisaaVarastoon(10);
         varasto.otaVarastosta(-10);
-        assertEquals(210, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(0, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
     @Test
